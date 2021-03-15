87a31b347cd7ad90cb41e23512dd4987534c8f4d
TheIronboy <66006359+TheIronboy@users.noreply.github.com>
2021-03-15 10:23:08 +0100

    Add files via upload

--- automaticMeet/Form1.cs
+++ automaticMeet/Form1.cs
@@ -22,9 +22,6 @@ namespace automaticMeet
         [System.Runtime.InteropServices.DllImport("user32.dll")]
         public static extern void mouse_event(int dwFlags, int dx, int dy, int cButtons, int dwExtraInfo);
 
-        public const int MOUSEEVENTF_LEFTDOWN = 0x02;
-        public const int MOUSEEVENTF_LEFTUP = 0x04;
-
         public Form1()
         {
             InitializeComponent();
@@ -49,8 +46,8 @@ namespace automaticMeet
         public static void LeftMouseClick(int xpos, int ypos)
         {
             SetCursorPos(xpos, ypos);
-            mouse_event(MOUSEEVENTF_LEFTDOWN, xpos, ypos, 0, 0);
-            mouse_event(MOUSEEVENTF_LEFTUP, xpos, ypos, 0, 0);
+            mouse_event(0x02, xpos, ypos, 0, 0);
+            mouse_event(0x04, xpos, ypos, 0, 0);
         }
 
         public async void button1_Click(object sender, EventArgs e)
