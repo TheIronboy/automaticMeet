b79f71543221133e1f5cc581f11bbea00e32e7cd
TheIronboy <66006359+TheIronboy@users.noreply.github.com>
2021-05-07 09:02:18 +0200

    Add files via upload

---
+++ automaticMeet/accountManager.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace automaticMeet
+{
+    public partial class Login : Form
+    {
+        string[] sessionFile = new string[3];
+
+        public void getSessionAndUserList()
+        {
+            using (StreamReader sr = File.OpenText(@"C:\automaticMeet\.session.txt"))
+            {
+                for (int i = 0; i < sessionFile.Length; i++)
+                {
+                    sessionFile[i] = sr.ReadLine();
+                }
+            }
+
+            string[] foundDirectory = Directory.GetDirectories(@"C:\automaticMeet\");
+
+            comboBox1.Items.Clear();
+
+            foreach (string directoryName in foundDirectory)
+                comboBox1.Items.Add(Path.GetFileName(directoryName));
+
+            if (Directory.Exists(@"C:\automaticMeet\" + sessionFile[0]) && sessionFile[2] == "True")
+            {
+                comboBox1.Text = sessionFile[0];
+                textBox1.Text = sessionFile[1];
+                checkBox1.Checked = true;
+            }
+            else
+            {
+                File.Create(@"C:\automaticMeet\.session.txt").Close();
+                comboBox1.Text = "";
+                textBox1.Text = "";
+            }
+        }
+
+        public Login()
+        {
+            InitializeComponent();
+
+            if (!Directory.Exists(@"C:\automaticMeet"))
+                Directory.CreateDirectory(@"C:\automaticMeet");
+
+            if (!File.Exists(@"C:\automaticMeet\.session.txt"))
+                File.Create(@"C:\automaticMeet\.session.txt").Close();
+
+            getSessionAndUserList();
+        }
+
+        bool passShown = false;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (!passShown)
+            {
+                passShown = true;
+                textBox1.UseSystemPasswordChar = false;
+                button1.Text = "Hide";
+            }
+            else if (passShown)
+            {
+                passShown = false;
+                textBox1.UseSystemPasswordChar = true;
+                button1.Text = "Show";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text != "" && textBox1.Text != "")
+            {
+                string inputUsername = comboBox1.Text, inputPassword = textBox1.Text, filePassword = "";
+
+                if (Directory.Exists(@"C:\automaticMeet\" + inputUsername))
+                {
+                    using (StreamReader sr = File.OpenText(@"C:\automaticMeet\" + inputUsername + @"\password.txt"))
+                    {
+                        filePassword = sr.ReadLine();
+                    }
+
+                    if (inputPassword != filePassword)
+                    {
+                        MessageBox.Show("Password errata.");
+                        return;
+                    }
+                }
+                else if (!Directory.Exists(@"C:\automaticMeet\" + inputUsername))
+                {
+                    if (inputUsername.IndexOf('@') != -1 && inputUsername.IndexOf(' ') == -1)
+                    {
+                        Directory.CreateDirectory(@"C:\automaticMeet\" + inputUsername + @"\codes");
+
+                        using (StreamWriter sw = File.CreateText(@"C:\automaticMeet\" + inputUsername + @"\password.txt"))
+                        {
+                            sw.WriteLine(inputPassword);
+                        }
+
+                        File.Create(@"C:\automaticMeet\" + inputUsername + @"\settings.txt").Close();
+
+                        MessageBox.Show("Rilevato nuovo utente, benvenuto!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Inserisci una mail valida.");
+                        return;
+                    }
+                }
+
+                using (StreamWriter sw = File.CreateText(@"C:\automaticMeet\.session.txt"))
+                {
+                    sw.WriteLine(inputUsername);
+                    sw.WriteLine(inputPassword);
+
+                    if (checkBox1.Checked)
+                        sw.WriteLine("True");
+                    else if (!checkBox1.Checked)
+                        sw.WriteLine("False");
+                }
+
+                this.Hide();
+                Form automaticMeet = new automaticMeet();
+
+                automaticMeet.Closed += (s, args) => this.Close();
+                automaticMeet.Show();
+            }
+            else
+                MessageBox.Show("Riempi tutti i campi.");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text != "" && textBox1.Text != "")
+            {
+                string inputUsername = comboBox1.Text, inputPassword = textBox1.Text, filePassword = "";
+
+                if (Directory.Exists(@"C:\automaticMeet\" + inputUsername))
+                {
+                    using (StreamReader sr = File.OpenText(@"C:\automaticMeet\" + inputUsername + @"\password.txt"))
+                    {
+                        filePassword = sr.ReadLine();
+                    }
+
+                    if (inputPassword == filePassword)
+                    {
+                        Directory.Delete(@"C:\automaticMeet\" + inputUsername, true);
+
+                        File.Create(@"C:\automaticMeet\.session.txt").Close();
+
+                        getSessionAndUserList();
+
+                        MessageBox.Show("Eliminato con successo!");
+                    }
+                    else
+                    {
+                        MessageBox.Show("Password errata.");
+                        return;
+                    }
+                }
+                else
+                    MessageBox.Show("Account non trovato.");
+            }
+            else
+                MessageBox.Show("Seleziona un account.");
+        }
+    }
+}
---
+++ automaticMeet/codeManager.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+
+namespace automaticMeet
+{
+    public partial class codeManager : Form
+    {
+        string[] sessionFile = new string[3];
+
+        public void getSessionAndCodes()
+        {
+            using (StreamReader sr = File.OpenText(@"C:\automaticMeet\.session.txt"))
+            {
+                for (int i = 0; i < sessionFile.Length; i++)
+                {
+                    sessionFile[i] = sr.ReadLine();
+                }
+            }
+
+            string[] fileEntries = Directory.GetFiles(@"C:\automaticMeet\" + sessionFile[0] + @"\codes");
+
+            comboBox1.Items.Clear();
+
+            foreach (string fileName in fileEntries)
+                comboBox1.Items.Add(Path.GetFileName(fileName.Remove(fileName.Length - 4)));
+        }
+
+        public codeManager()
+        {
+            InitializeComponent();
+
+            getSessionAndCodes();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text != "" && textBox1.Text != "")
+            {
+                string codeName = comboBox1.Text, actualCode = textBox1.Text;
+
+                if (!File.Exists(@"C:\automaticMeet\" + sessionFile[0] + @"\codes\" + codeName + ".txt"))
+                {
+                    using (StreamWriter sw = File.CreateText(@"C:\automaticMeet\" + sessionFile[0] + @"\codes\" + codeName + ".txt"))
+                    {
+                        sw.WriteLine(actualCode);
+                    }
+
+                    MessageBox.Show("Codice creato con successo!");
+
+                    this.Close();
+                }
+                else
+                    MessageBox.Show("Codice già esistente.");
+            }
+            else
+                MessageBox.Show("Riempi tutti i campi.");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (comboBox1.Text != "")
+            {
+                string codeName = comboBox1.Text;
+
+                if (File.Exists(@"C:\automaticMeet\" + sessionFile[0] + @"\codes\" + codeName + ".txt"))
+                {
+                    File.Delete(@"C:\automaticMeet\" + sessionFile[0] + @"\codes\" + codeName + ".txt");
+
+                    getSessionAndCodes();
+
+                    MessageBox.Show("Eliminato con successo!");
+
+                    this.Close();
+                }
+                else
+                    MessageBox.Show("Codice non trovato.");
+            }
+            else
+                MessageBox.Show("Seleziona un codice.");
+        }
+    }
+}
